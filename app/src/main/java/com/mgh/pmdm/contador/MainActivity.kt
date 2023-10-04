41126aabc23e210ad0e3be31d7c86c6d8bbdece2
Usuario <saragl728@gmail.com>
2023-10-04 09:33:46 +0200

    Intento enviarlo

---
+++ app/src/main/java/com/mgh/pmdm/contador/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.mgh.pmdm.contador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    var contador=0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Referencia al TextView
+        val textViewContador=findViewById<TextView>(R.id.texto)
+
+        // Inicializamos el TextView con el contador a 0
+        textViewContador.setText(contador.toString())
+
+        // Referencia al botón
+        val btAdd=findViewById<Button>(R.id.btAdd)
+
+        // Asociaciamos una expresióin lambda como
+        // respuesta (callback) al evento Clic sobre
+        // el botón
+        btAdd.setOnClickListener {
+            contador++
+            textViewContador.setText(contador.toString())
+        }
+
+        val btMenos = findViewById<Button>(R.id.btMenos)
+
+        btMenos.setOnClickListener {
+            contador--
+            textViewContador.setText(contador.toString())
+        }
+
+    }
+}
